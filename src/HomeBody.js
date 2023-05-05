5eb74bd40cf52082a14f168c92ae34127089e84f
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2023-05-05 13:40:20 -0300

    Add Cats and Dogs project.

--- src/HomeBody.js
+++ src/HomeBody.js
@@ -14,21 +14,25 @@ function HomeBody() {
             </div>
 
             <h1>&#128187; -  Projetos: </h1>
-                <div className="ArticleLink">
-                    <a href="https://brdata.online">BRData Collector.</a>
-                    <p>Portal para pesquisa sobre Gastos, Contratos e Licitações de instituições do Governo Federal. Também é possível pesquisar sobre Empresas que prestam serviço para Entidades Federais.</p>
-                    <p>Sistema de código aberto e disponível no <a style={{fontSize:"1.2em"}} href="https://github.com/carlosplf/br_data_inspector">Github</a>.</p>
+                <div className="ArticleSession">
+                    <img className="ArticleIcon" src="./images/br-data-square.jpeg"/>
+                    <div className="ArticleDetails">
+                        <a href="https://brdata.site">BRData Collector.</a>
+                        <p>Portal para pesquisa sobre Gastos, Contratos e Licitações de instituições do Governo Federal. Também é possível pesquisar sobre Empresas que prestam serviço para Entidades Federais.</p>
+                        <p>Sistema de código aberto e disponível no <a style={{fontSize:"1.2em"}} href="https://github.com/carlosplf/br_data_inspector">Github</a>.</p>
+                    </div>
+                </div>
+
+                <div className="ArticleSession">
+                    <img className="ArticleIcon" src="./images/cats-dogs-square.jpeg"/>
+                    <div className="ArticleDetails">
+                        <a href="https://ml.carlosplf.com.br">Cats and Dogs classifier.</a>
+                        <p>Modelo de Machine Learning classificador de Gatos e Cachorros com base em imagem. Através da página, usuários podem submeter imagens para classificação.</p>
+                        <p>Sistema de código aberto e disponível no <a style={{fontSize:"1.2em"}} href="https://github.com/carlosplf/tf-cats-and-dogs">Github</a>.</p>
+                    </div>
                 </div>
             
-            <h1>&#127916; - Mídia: </h1>
-
-                <div className="ArticleLink">
-                    <a href="https://open.spotify.com/episode/6cng4AO3j3Nqci37KKjWsJ?si=d7c8028da8e14147">Podcast - Raccoonversa: Google Analytics 4.</a>
-                </div>
-                
-                <div className="ArticleLink">
-                    <a href="https://open.spotify.com/episode/5O3kAtVo2uQAY6jOzN93q6?si=bbfbef8351e04361">Podcast - Raccoonversa: Inovação Tech no Marketing Digital.</a>
-                </div>
+            
         </div>
     );
 }
