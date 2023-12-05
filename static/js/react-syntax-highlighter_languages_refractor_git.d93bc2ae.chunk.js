d4c32dbed3c85a3622b95c458b0835e5fcd71bb2
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2023-12-05 15:46:06 -0300

    Testing with GitHub Pages

---
+++ static/js/react-syntax-highlighter_languages_refractor_git.d93bc2ae.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_site=self.webpackChunkpersonal_site||[]).push([[2564],{787:function(e){function s(e){e.languages.git={comment:/^#.*/m,deleted:/^[-\u2013].*/m,inserted:/^\+.*/m,string:/("|')(?:\\.|(?!\1)[^\\\r\n])*\1/m,command:{pattern:/^.*\$ git .*$/m,inside:{parameter:/\s--?\w+/m}},coord:/^@@.*@@$/m,commit_sha1:/^commit \w{40}$/m}}e.exports=s,s.displayName="git",s.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_git.d93bc2ae.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_hsts.fa30063b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_site=self.webpackChunkpersonal_site||[]).push([[3140],{1213:function(e){function a(e){e.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}e.exports=a,a.displayName="hsts",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.fa30063b.chunk.js.map
