02fbb92cf09ba7a1bbfe12c54ff2ecac36d29c83
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2023-12-05 18:05:43 -0300

    Change header and apply blur effect.

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,11 @@ function Header() {
 
     return(
         <div className="Header">
+          <div className="background-image">
             <Link to="/"><h1 id="SiteName"> Carlos Pereira Lopes  </h1></Link>
             <p id="Subtitle">Meus projetos e anotações sobre Tecnologia. </p>
-        </div>
+          </div>
+      </div>
     );
 }
 
