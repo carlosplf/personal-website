f0a3b25aa2cc456df63a80a910f00e2144bda4b3
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2024-03-04 11:36:54 -0300

    Remove profile pic

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,6 @@ function Header() {
     return(
         <div className="Header">
           <div className="background-image">
-            <Link to="/"> <img id="profile-pic" src="images/profile-raccoon.png"/></Link>
             <div className="author-name">
               <Link to="/"><h1 id="SiteName"> Carlos Pereira Lopes </h1></Link>
               <p id="Subtitle">Meus projetos e anotações sobre Tecnologia. </p>
